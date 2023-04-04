b04cb30e37685cde3d996c51ce8d5db58d1c8d1f
Vero Conesa <vero.gconesa@gmail.com>
2023-04-04 15:31:33 +0200

    ⚡️ perf Added taskServices to connect with DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,76 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Todos } from './components/Todos'
 import { TODO_FILTERS } from './const'
 import { type TodoId, type Todo as TodoType, type FilterValue, type TodoTitle } from './types'
+import { Login } from './components/Login'
+import { Register } from './components/Register'
+import { getAllTask, createTask, deleteTask, completeTask } from './services/Tasks/TaskServices'
 
-const mockTodos = [
-  {
-    id: '1',
-    title: 'todo 1',
-    completed: true
-  },
-  {
-    id: '2',
-    title: 'todo 2',
-    completed: false
-  },
-  {
-    id: '3',
-    title: 'todo 3',
-    completed: false
-  }
-]
+// const mockTodos = [
+//   {
+//     id: '1',
+//     title: 'todo 1',
+//     completed: true
+//   },
+//   {
+//     id: '2',
+//     title: 'todo 2',
+//     completed: false
+//   },
+//   {
+//     id: '3',
+//     title: 'todo 3',
+//     completed: false
+//   }
+// ]
 
 const App = (): JSX.Element => {
-  const [todos, setTodos] = useState(mockTodos)
+  const [todos, setTodos] = useState<TodoType[]>([])
   const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
 
+  useEffect(() => {
+    getAllTask().then(listOfTodos => {
+      setTodos(listOfTodos)
+    })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
+
+  // const handleRemove = ({ id }: TodoId): void => {
+  //   const newTodos = todos.filter(todo => todo.id !== id)
+  //   setTodos(newTodos)
+  // }
+
   const handleRemove = ({ id }: TodoId): void => {
-    const newTodos = todos.filter(todo => todo.id !== id)
-    setTodos(newTodos)
+    deleteTask({ id })
+      .then(() => {
+        const newTodos = todos.filter(todo => todo.id !== id)
+        setTodos(newTodos)
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
   const handleCompleted = (
     { id, completed }: Pick<TodoType, 'id' | 'completed'>
   ): void => {
-    const newTodos = todos.map(todo => {
-      return (todo.id === id
-        ? { ...todo, completed }
-        : todo
-      )
-    })
-    setTodos(newTodos)
+    completeTask({ id, completed })
+      .then(() => {
+        const newTodos = todos.map(todo => {
+          return (todo.id === id
+            ? { ...todo, completed }
+            : todo
+          )
+        })
+        setTodos(newTodos)
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
   const handleFilterChange = (filter: FilterValue): void => {
@@ -49,8 +78,17 @@ const App = (): JSX.Element => {
   }
 
   const handleRemoveAllCompleted = (): void => {
-    const newTodos = todos.filter(todo => !todo.completed)
-    setTodos(newTodos)
+    const completedTasks = todos.filter(todo => todo.completed)
+    Promise.all(completedTasks.map(async ({ id }) => { await deleteTask({ id }) }))
+      .then(() => {
+        const newTodos = todos.filter(todo => !todo.completed)
+        setTodos(newTodos)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    // const newTodos = todos.filter(todo => !todo.completed)
+    // setTodos(newTodos)
   }
 
   const activeCount = todos.filter(todo => !todo.completed).length
@@ -63,16 +101,29 @@ const App = (): JSX.Element => {
   })
 
   const handleAddToDo = ({ title }: TodoTitle): void => {
-    const newToDo = {
-      title,
-      id: crypto.randomUUID(),
-      completed: false
-    }
-    const newTodos = [...todos, newToDo]
-    setTodos(newTodos)
+    createTask({ title })
+      .then(res => {
+        const newToDos = todos.concat(res)
+        setTodos(newToDos)
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
+  const handleLogin = (): void => {
+
+  }
+  const handleRegister = (): void => {
+
+  }
   return (
+    <>
+      <div className='user'>
+        <Login handleLogin={handleLogin} href='/login'></Login>
+        <h3>|</h3>
+        <Register handleRegister={handleRegister} href='/Register'></Register>
+      </div>
     <div className='todoapp'>
       <Header newToDo={handleAddToDo}/>
       <Todos
@@ -88,7 +139,7 @@ const App = (): JSX.Element => {
       onClearCompleted = {handleRemoveAllCompleted}
       />
     </div>
-
+    </>
   )
 }
 
---
+++ src/services/Tasks/TaskServices.ts
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { type Todo as TodoType, type TodoTitle, type TodoId } from '../../types'
+
+const URL = 'http://localhost:5001/api/task'
+
+export const getAllTask = async (): Promise<TodoType[]> => {
+  try {
+    const response = await axios.get<TodoType[]>(URL)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const createTask = async ({ title }: TodoTitle): Promise<TodoType[]> => {
+  const newToDo = {
+    title,
+    completed: false,
+    user_id: 1
+  }
+
+  try {
+    const response = await axios.post<TodoType[]>(URL, newToDo)
+    console.log({ response })
+    return response.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const deleteTask = async ({ id }: TodoId): Promise<void> => {
+  try {
+    await axios.delete(`${URL}/${id.toString()}`)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const completeTask = async (
+  { id, completed }: Pick<TodoType, 'id' | 'completed'>): Promise<void> => {
+  try {
+    await axios.put(`${URL}/${id.toString()}`, { completed })
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
