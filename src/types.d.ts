c4cc4cb09eefa49d8b2c92d9520c12e4075c30e7
Vero Conesa <vero.gconesa@gmail.com>
2023-04-04 15:30:45 +0200

    ⚡️ perf Added user_id to todoType

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,13 +1,15 @@
 import { type TODO_FILTERS } from './const'
 export interface Todo {
-  id: string
+  id: number
   title: string
   completed: boolean
+  user_id: number
 }
 
 export type TodoTitle = Pick<Todo, 'title'>
 export type TodoId = Pick<Todo, 'id'>
 export type TodoCompleted = Pick<Todo, 'completed'>
+export type TodoUser_id = Pick<Todo, 'user_id'>
 
 export type ListOfTodos = Todo[]
 
