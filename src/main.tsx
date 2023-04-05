8fb2ca0c39f85a8478419020276c4e0754625288
Vero Conesa <vero.gconesa@gmail.com>
2023-04-05 16:22:16 +0200

    🐛 fix Added ReactRouterDom

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'todomvc-app-css/index.css'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 )
