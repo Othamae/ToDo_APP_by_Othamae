77a551100218837a65a85425ab8499c29f8ce8e1
Vero Conesa <vero.gconesa@gmail.com>
2023-04-11 23:27:14 +0200

    🆕 feat Added userId UseState

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { ToDos } from './pages/ToDos'
 import { Register } from './pages/Register'
 import { User } from './components/User'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import { type UserId } from './types'
 
 const App = (): JSX.Element => {
+  const [userId, setUserId] = useState < UserId >({ id: 0 })
+  console.log({ userId })
   return (
     <>
       <div className='user'>
-        <User ></User>
+        <User userId = {userId}></User>
       </div>
 
     <Routes>
-      <Route path='/' element={<ToDos/>}/>
-    <Route path='/login' element={<Login />} />
+      <Route path='/' element={<ToDos userId = {userId}/>}/>
+    <Route path='/login' element={<Login setUserId={setUserId}/>} />
     <Route path='/register' element={<Register />} />
     </Routes>
     </>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -6,10 +6,12 @@ interface Props {
   onRemoveTodo: ({ id }: TodoId) => void
   onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
   onEditTodo: ({ id }: TodoId, { title }: TodoTitle) => void
+
 }
 
 export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompleteTodo, onEditTodo }) => {
   const sortedTodos = todos.sort((a, b) => a.id - b.id)
+
   return (
         <ul className='todo-list'>
             {sortedTodos.map(todo => (
@@ -19,7 +21,7 @@ export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompleteTo
                     id= {todo.id}
                     title= {todo.title}
                     completed= {todo.completed}
-                    user_id={todo.user_id}
+                    userId={todo.userId}
                     onToggleCompleteTodo = {onToggleCompleteTodo}
                     onRemoveTodo={onRemoveTodo}
                     onEditTodo={onEditTodo}
