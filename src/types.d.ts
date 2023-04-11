693a0c72cb1a1e10fe42c302408dfb941dc2abac
Vero Conesa <vero.gconesa@gmail.com>
2023-04-11 23:26:05 +0200

    🏗️  build Added User types

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,14 +3,26 @@ export interface Todo {
   id: number
   title: string
   completed: boolean
-  user_id: number
+  userId: number
 }
 
 export type TodoTitle = Pick<Todo, 'title'>
 export type TodoId = Pick<Todo, 'id'>
 export type TodoCompleted = Pick<Todo, 'completed'>
-export type TodoUser_id = Pick<Todo, 'user_id'>
+export type TodoUser_id = Pick<Todo, 'userId'>
 
 export type ListOfTodos = Todo[]
 
 export type FilterValue = typeof TODO_FILTERS[keyof typeof TODO_FILTERS]
+
+export interface User {
+  id: number
+  email: string
+  password: string
+}
+
+export type UserEmail = Pick<User, 'email'>
+export type UserPassword = Pick<User, 'password'>
+export type UserId = Pick<User, 'id'>
+
+export type Notification = string
