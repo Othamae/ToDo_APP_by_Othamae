cdfdb732d27b7252eeb0821287f28b8c25e5c075
Vero Conesa <vero.gconesa@gmail.com>
2023-04-11 23:22:34 +0200

    🐛 fix Fixed type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
-import { type TodoTitle } from '../types'
+import { type TodoTitle, type UserId as UserIdType } from '../types'
 import { CreateToDo } from './CreateToDo'
 
 interface Props {
-  newToDo: ({ title }: TodoTitle) => void
+  newToDo: ({ title }: TodoTitle, { id }: UserIdType) => void
+  userId: UserIdType
 }
 
-export const Header: React.FC<Props> = ({ newToDo }) => {
+export const Header: React.FC<Props> = ({ newToDo, userId }) => {
   return (
     <header className="header">
         <h1>ToDo List</h1>
-        <CreateToDo saveToDo={newToDo} />
+        <CreateToDo saveToDo={newToDo} userId = {userId}/>
     </header>
 
   )
