8d4a130ea717747261fac5b6213165561593c051
Vero Conesa <vero.gconesa@gmail.com>
2023-04-11 23:23:52 +0200

    🆕 feat Added Props

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,19 +1,27 @@
 
 import { Link } from 'react-router-dom'
+import { type UserId as UserIdType } from '../types'
 
-export const User: React.FC = () => {
-  const isLogged = false
+interface Props {
+  userId: UserIdType
+}
+
+export const User: React.FC<Props> = ({ userId }) => {
+  const { id } = userId
+
+  const isLogged = id !== 0
 
   return (
     <>
     <div className='user'>
     {isLogged
       ? (<Link to='/logout' >Logout</Link>)
-      : (<Link to='/login' >Login</Link>)
-    }
-    <h3>|</h3>
-    <Link to='/register' >Register</Link>
-
+      : (<>
+          <Link to='/login' >Login</Link>
+          <h3>|</h3>
+          <Link to='/register' >Register</Link>
+          </>)
+}
  </div>
     </>
   )
