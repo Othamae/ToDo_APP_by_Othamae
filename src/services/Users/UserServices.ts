c82ac9c93883afba24bff710d0acba389105ae1c
Vero Conesa <vero.gconesa@gmail.com>
2023-04-12 13:10:00 +0200

    ⚡️ perf Added localStorage

--- src/services/Users/UserServices.ts
+++ src/services/Users/UserServices.ts
@@ -1,11 +1,24 @@
 import axios from 'axios'
-import { type UserPassword, type UserEmail, type User as UserType } from '../../types'
+import { type UserPassword, type UserEmail, type User as UserType, type UserName } from '../../types'
 
 const URL = 'http://localhost:5001/api/user'
 
 export const LoginUser = async ({ email }: UserEmail, { password }: UserPassword): Promise<UserType> => {
   try {
     const response = await axios.post(`${URL}/login`, { email, password })
+    console.log({ response })
+    const user = response.data
+    window.localStorage.setItem('UserLogged', JSON.stringify(user))
+    return user
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const RegisterUser = async ({ email }: UserEmail, { password }: UserPassword, { name }: UserName): Promise<UserType> => {
+  try {
+    const response = await axios.post(`${URL}/register`, { email, password, name })
     console.log({ response })
     return response.data
   } catch (error) {
