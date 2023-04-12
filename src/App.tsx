76ba07c04b8167b71840a3c269150f22def2ae18
Vero Conesa <vero.gconesa@gmail.com>
2023-04-12 13:25:39 +0200

    🆕 feat Added localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,32 @@ import { ToDos } from './pages/ToDos'
 import { Register } from './pages/Register'
 import { User } from './components/User'
 import { Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
-import { type UserId } from './types'
+import { useEffect, useState } from 'react'
+import { type UserName as UserNameType, type UserId as UserIdType } from './types'
 
 const App = (): JSX.Element => {
-  const [userId, setUserId] = useState < UserId >({ id: 0 })
-  console.log({ userId })
+  const [userId, setUserId] = useState < UserIdType >({ id: 0 })
+  const [userName, setUserName] = useState< UserNameType >({ name: '' })
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('UserLogged')
+    if (loggedUserJSON != null) {
+      const user = JSON.parse(loggedUserJSON)
+      const { name, id } = user
+      setUserId({ id })
+      setUserName({ name })
+    }
+  }, [])
+
   return (
     <>
       <div className='user'>
-        <User userId = {userId}></User>
+        <User userId = {userId} userName={userName} setUserId={setUserId} setUserName={setUserName}></User>
       </div>
 
     <Routes>
       <Route path='/' element={<ToDos userId = {userId}/>}/>
-    <Route path='/login' element={<Login setUserId={setUserId}/>} />
+    <Route path='/login' element={<Login setUserId={setUserId} setUserName = {setUserName}/>} />
     <Route path='/register' element={<Register />} />
     </Routes>
     </>
