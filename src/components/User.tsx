df7b86a1887a885d11d2f211ba29fd2a2dda8462
Vero Conesa <vero.gconesa@gmail.com>
2023-04-12 13:23:05 +0200

    ⚡️ perf Added Logout

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,21 +1,42 @@
 
-import { Link } from 'react-router-dom'
-import { type UserId as UserIdType } from '../types'
+import { Link, useNavigate } from 'react-router-dom'
+import { type UserName as UserNameType, type UserId as UserIdType } from '../types'
+import { useState, useEffect } from 'react'
 
 interface Props {
   userId: UserIdType
+  userName: UserNameType
+  setUserId: React.Dispatch<React.SetStateAction<UserIdType>>
+  setUserName: React.Dispatch<React.SetStateAction<UserNameType>>
 }
 
-export const User: React.FC<Props> = ({ userId }) => {
+export const User: React.FC<Props> = ({ userId, userName, setUserId, setUserName }) => {
   const { id } = userId
+  const { name } = userName
+  const navigate = useNavigate()
+  const getUserId = (): number => id
 
-  const isLogged = id !== 0
+  const [isLogged, setIsLogged] = useState(() => getUserId() !== 0)
+
+  useEffect(() => {
+    setIsLogged(getUserId() !== 0)
+  }, [userId])
+  const handleLogout = (): void => {
+    window.localStorage.removeItem('UserLogged')
+    setIsLogged(false)
+    setUserName({ name: '' })
+    setUserId({ id: 0 })
+    navigate('/', { state: { userId: 0 }, replace: true })
+  }
 
   return (
     <>
     <div className='user'>
     {isLogged
-      ? (<Link to='/logout' >Logout</Link>)
+      ? (<>
+      <h3>{name}</h3>
+      <button className='logout' onClick={handleLogout} >Logout</button>
+      </>)
       : (<>
           <Link to='/login' >Login</Link>
           <h3>|</h3>
